738cdb3335d77823dea16fc8ccada427dab22e6b
alamdilshad87 <dilshadand@gmail.com>
2025-11-12 12:29:58 +0530

    Added Spacing

--- AddressBook/AddressBook/Book.cs
+++ AddressBook/AddressBook/Book.cs
@@ -14,14 +14,22 @@ namespace AddressBook
         public void AddContact()
         {
             Contact contact = new Contact();
-            Console.Write("Enter First Name: "); contact.FirstName = Console.ReadLine();
-            Console.Write("Enter Last Name: "); contact.LastName = Console.ReadLine();
-            Console.Write("Enter Address: "); contact.Address = Console.ReadLine();
-            Console.Write("Enter City: "); contact.City = Console.ReadLine();
-            Console.Write("Enter State: "); contact.State = Console.ReadLine();
-            Console.Write("Enter Zip: "); contact.Zip = Console.ReadLine();
-            Console.Write("Enter Phone: "); contact.PhoneNumber = Console.ReadLine();
-            Console.Write("Enter Email: "); contact.Email = Console.ReadLine();
+            Console.Write("Enter First Name: "); 
+            contact.FirstName = Console.ReadLine();
+            Console.Write("Enter Last Name: "); 
+            contact.LastName = Console.ReadLine();
+            Console.Write("Enter Address: "); 
+            contact.Address = Console.ReadLine();
+            Console.Write("Enter City: "); 
+            contact.City = Console.ReadLine();
+            Console.Write("Enter State: "); 
+            contact.State = Console.ReadLine();
+            Console.Write("Enter Zip: "); 
+            contact.Zip = Console.ReadLine();
+            Console.Write("Enter Phone: "); 
+            contact.PhoneNumber = Console.ReadLine();
+            Console.Write("Enter Email: "); 
+            contact.Email = Console.ReadLine();
 
             contacts.Add(contact);
             Console.WriteLine("\nContact added successfully!\n");
