c8eccb513ec4898306338f5a1870b208ebe66f6b
alamdilshad87 <dilshadand@gmail.com>
2025-12-23 16:33:17 +0530

    Refactor with dictionary

---
+++ AddressBook/AddressBook/AddressBookSystem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class AddressBookSystem
+    {
+        private Dictionary<string, Book> books = new();
+
+        public void AddAddressBook(string name)
+        {
+            if (!books.ContainsKey(name))
+            {
+                books[name] = new Book();
+                Console.WriteLine($"Address Book '{name}' created.");
+            }
+            else
+            {
+                Console.WriteLine("Address Book already exists!");
+            }
+        }
+
+        public Book GetAddressBook(string name)
+        {
+            return books.ContainsKey(name) ? books[name] : null;
+        }
+
+        public void DisplayAddressBooks()
+        {
+            Console.WriteLine("\nAvailable Address Books:");
+            foreach (var key in books.Keys)
+                Console.WriteLine($"- {key}");
+        }
+    }
+}
+
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -5,49 +5,63 @@ public class Program
     public static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Address Book Program!");
-        Book book = new Book();
+        AddressBookSystem system = new AddressBookSystem();
+        Book currentBook = null;
 
         bool keepAdding = true;
         while (keepAdding)
         {
-            Console.WriteLine("\n1. Add New Contact");
-            Console.WriteLine("2. Display All Contacts");
-            Console.WriteLine("3. Edit Contact");
-            Console.WriteLine("4. Delete Contact");
-            Console.WriteLine("5. Exit");
+            Console.WriteLine("\n0. Create Address Book");
+            Console.WriteLine("1. Select Address Book");
+            Console.WriteLine("2. Add New Contact");
+            Console.WriteLine("3. Display All Contacts");
+            Console.WriteLine("4. Edit Contact");
+            Console.WriteLine("5. Delete Contact");
+            Console.WriteLine("6. Exit");
 
             Console.Write("Enter your choice: ");
             string choice = Console.ReadLine();
 
             switch (choice)
             {
+                case "0":
+                    Console.Write("Enter Address Book Name: ");
+                    system.AddAddressBook(Console.ReadLine());
+                    break;
                 case "1":
-                    book.AddContact();
+                    system.DisplayAddressBooks();
+                    Console.Write("Enter Address Book Name: ");
+                    currentBook = system.GetAddressBook(Console.ReadLine());
+                    if (currentBook == null)
+                        Console.WriteLine("Address Book not found!");
                     break;
                 case "2":
-                    book.DisplayContacts();
+                    currentBook.AddContact();
                     break;
                 case "3":
-                    if(book.ContactCount==0)
+                    currentBook.DisplayContacts();
+                    break;
+                case "4":
+                    if(currentBook.ContactCount==0)
                     {
                         Console.WriteLine("No contacts available to edit.");
                         break;
                     }
                     Console.Write("Enter the First Name of the contact to edit: ");
                     string name = Console.ReadLine();
-                    book.EditContact(name);
+                    currentBook.EditContact(name);
                     break;
-                case "4":
-                    if (book.ContactCount == 0)
+                case "5":
+                    if (currentBook.ContactCount == 0)
                     {
                         Console.WriteLine("No contacts available to delete.");
                         break;
                     }
                     Console.Write("Enter the First Name of the contact to delete: ");
                     string deleteName = Console.ReadLine();
-                    book.DeleteContact(deleteName);
+                    currentBook.DeleteContact(deleteName);
                     break;
-                case "5":
+                case "6":
                     keepAdding = false;
                     Console.WriteLine("\nThank you for using Address Book!");
                     break;
